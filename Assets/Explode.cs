1e2b7cc11ffd0708e4466dfc92be4e9874029a9d
nazninshishir <naznin.shishir1@ucalgary.ca>
2024-12-12 22:07:08 -0700

    Final version

---
+++ Assets/Explode.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public GameObject explosion;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "zombi")
+        {
+            Destroy(collision.transform.gameObject); // destroy spider
+            Instantiate(explosion, collision.transform.position, collision.transform.rotation);
+        }
+    }
+}
+
---
+++ FinalProject_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,157 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 UnityEngine.GameObject AnchorCreator::get_AnchorPrefab()
+extern void AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A (void);
+// 0x00000002 System.Void AnchorCreator::set_AnchorPrefab(UnityEngine.GameObject)
+extern void AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF (void);
+// 0x00000003 System.Void AnchorCreator::RemoveAllAnchors()
+extern void AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21 (void);
+// 0x00000004 System.Void AnchorCreator::Awake()
+extern void AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E (void);
+// 0x00000005 System.Void AnchorCreator::Update()
+extern void AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0 (void);
+// 0x00000006 System.Void AnchorCreator::.ctor()
+extern void AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E (void);
+// 0x00000007 System.Void AnchorCreator::.cctor()
+extern void AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4 (void);
+// 0x00000008 System.Single ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D (void);
+// 0x00000009 System.Void ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110 (void);
+// 0x0000000A System.Void ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA (void);
+// 0x0000000B System.Void ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0 (void);
+// 0x0000000C System.Void ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8 (void);
+// 0x0000000D System.Void ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F (void);
+// 0x0000000E System.Void ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5 (void);
+// 0x0000000F System.Void ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6 (void);
+// 0x00000010 System.Void ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3 (void);
+// 0x00000011 System.Void Explode::OnCollisionEnter(UnityEngine.Collision)
+extern void Explode_OnCollisionEnter_m1C3277FDC7C28E9593096729FFF647316B1D5BEB (void);
+// 0x00000012 System.Void Explode::.ctor()
+extern void Explode__ctor_m87E0F75692364C98C2BBCF984D64D9E198F126A6 (void);
+// 0x00000013 System.Void PlaceOnPlane::Start()
+extern void PlaceOnPlane_Start_mAB5B71328132068292CC9339EAE5F8DD40249822 (void);
+// 0x00000014 System.Void PlaceOnPlane::Update()
+extern void PlaceOnPlane_Update_m4BAC7B0437CB4CB61EF9C23849AC096E5EE7C2B7 (void);
+// 0x00000015 System.Void PlaceOnPlane::UpdatePlacementIndicator()
+extern void PlaceOnPlane_UpdatePlacementIndicator_mF1F7035BA065DBE83B234304A88E48CF1F87FB86 (void);
+// 0x00000016 System.Void PlaceOnPlane::UpdatePlacementPose()
+extern void PlaceOnPlane_UpdatePlacementPose_m8205754D734A9293F77A773F0D7A7DA532B38FA5 (void);
+// 0x00000017 System.Void PlaceOnPlane::ARPlaceObject()
+extern void PlaceOnPlane_ARPlaceObject_m78B948C719A114F346D548CDDCC1FBB14AAFAF0F (void);
+// 0x00000018 System.Void PlaceOnPlane::.ctor()
+extern void PlaceOnPlane__ctor_mEAC8D0276C9BC36A8A633F162E3F8EBF96D6255C (void);
+// 0x00000019 System.Void Shoot::Update()
+extern void Shoot_Update_m8BAF074111542636D4F80A67B54B2AF3CFD14BFE (void);
+// 0x0000001A System.Void Shoot::.ctor()
+extern void Shoot__ctor_m9CC0333342223937F83B34DB4109B3101CCB3AA9 (void);
+// 0x0000001B System.Void PlacementIndicator::Start()
+extern void PlacementIndicator_Start_m92791E0A4D6E907C1BA32A25E07872A9A2E6C9D7 (void);
+// 0x0000001C System.Void PlacementIndicator::Update()
+extern void PlacementIndicator_Update_mCC16B34A0C5216C7A2D0E61A9B2CD7AC068703B9 (void);
+// 0x0000001D System.Void PlacementIndicator::.ctor()
+extern void PlacementIndicator__ctor_mA2BDF502002E94CB872C1398E6506197E238AE09 (void);
+static Il2CppMethodPointer s_methodPointers[29] = 
+{
+	AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A,
+	AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF,
+	AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21,
+	AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E,
+	AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0,
+	AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E,
+	AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4,
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110,
+	ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5,
+	ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6,
+	ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3,
+	Explode_OnCollisionEnter_m1C3277FDC7C28E9593096729FFF647316B1D5BEB,
+	Explode__ctor_m87E0F75692364C98C2BBCF984D64D9E198F126A6,
+	PlaceOnPlane_Start_mAB5B71328132068292CC9339EAE5F8DD40249822,
+	PlaceOnPlane_Update_m4BAC7B0437CB4CB61EF9C23849AC096E5EE7C2B7,
+	PlaceOnPlane_UpdatePlacementIndicator_mF1F7035BA065DBE83B234304A88E48CF1F87FB86,
+	PlaceOnPlane_UpdatePlacementPose_m8205754D734A9293F77A773F0D7A7DA532B38FA5,
+	PlaceOnPlane_ARPlaceObject_m78B948C719A114F346D548CDDCC1FBB14AAFAF0F,
+	PlaceOnPlane__ctor_mEAC8D0276C9BC36A8A633F162E3F8EBF96D6255C,
+	Shoot_Update_m8BAF074111542636D4F80A67B54B2AF3CFD14BFE,
+	Shoot__ctor_m9CC0333342223937F83B34DB4109B3101CCB3AA9,
+	PlacementIndicator_Start_m92791E0A4D6E907C1BA32A25E07872A9A2E6C9D7,
+	PlacementIndicator_Update_mCC16B34A0C5216C7A2D0E61A9B2CD7AC068703B9,
+	PlacementIndicator__ctor_mA2BDF502002E94CB872C1398E6506197E238AE09,
+};
+static const int32_t s_InvokerIndices[29] = 
+{
+	5133,
+	4155,
+	5228,
+	5228,
+	5228,
+	5228,
+	8278,
+	5174,
+	4188,
+	5228,
+	5228,
+	5228,
+	4049,
+	4155,
+	5228,
+	8278,
+	4155,
+	5228,
+	5228,
+	5228,
+	5228,
+	5228,
+	5228,
+	5228,
+	5228,
+	5228,
+	5228,
+	5228,
+	5228,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	29,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
